f003e63243b1a3c31151d537fd52567a0ee8e6b1
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-07-14 23:18:57 -0300

    fizemos algum progresso com o tratamento dos dados
    mas ainda nos resta:
      - eliminar as linhas que apenas contem informacoes sobre os campus
      - algumas linhas nao possuem separadores entre curso e horario da
        disciplina

--- pkgs/core.py
+++ pkgs/core.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import re
-
 from pathlib import Path
 
 import pdfplumber
@@ -11,10 +10,10 @@ from unidecode import unidecode
 disciplinas = re.compile(r"DISCIPLINAS")
 campus = re.compile(r"CAMPUS (APARECIDA|COLEMAR|SAMAMBAIA)")
 horario = re.compile(r"([0-9]{1,3}[MTN][0-9]{2,4})")
-
-
-def pdf_exists(PDFfile):
-    return Path(PDFfile).is_file()
+separador = re.compile(r"\s*-\s*")
+APARECIDA = re.compile(r"CAMPUS APARECIDA(.*)CAMPUS COLEMAR", re.S)
+COLEMAR = re.compile(r"CAMPUS COLEMAR(.*)CAMPUS SAMAMBAIA", re.S)
+SAMAMBAIA = re.compile(r"CAMPUS SAMAMBAIA(.*)", re.S)
 
 
 def parsed_row(R):
@@ -44,32 +43,50 @@ def row_should_not_be_excluded(R):
     return should_not_be_excluded
 
 
-def parse_PDF(PDFfile):
-    stream = ""
-    with pdfplumber.open(PDFfile) as PDF:
-        N = len(PDF.pages)
-        for i in range(N):
-            table = [
-                R
-                for R in parsed_table(PDF.pages[i].extract_table())
-                if row_should_not_be_excluded(R)
-            ]
-            for R in table:
-                for C in R:
-                    C1 = disciplinas.search(C)
-                    C2 = campus.search(C)
-                    C3 = horario.match(C)
-                    if (C1 or C2 or C3):
-                        stream += C + "\n"
-                    else:
-                        stream += C + " "
-    return stream
+def parse_PDF(ano, semestre):
+    PDFfile = f"pdfs/{ano}-{semestre}.pdf"
+    CSVfile = f"brew/{ano}-{semestre}.csv"
+    if Path(PDFfile).is_file():
+        stream = ""
+        with pdfplumber.open(PDFfile) as PDF:
+            N = len(PDF.pages)
+            for i in range(N):
+                table = [
+                    R
+                    for R in parsed_table(PDF.pages[i].extract_table())
+                    if row_should_not_be_excluded(R)
+                ]
+                for R in table:
+                    for C in R:
+                        C1 = disciplinas.search(C)
+                        C2 = campus.search(C)
+                        C3 = horario.match(C)
+                        if C1 or C2 or C3:
+                            stream += C + "\n"
+                        else:
+                            stream += C + " "
+        stream = separador.sub(r";", stream)
+        aparecida = re.sub(
+            r"([0-9]{1,3}[MTN][0-9]{2,4})",
+            r"\1;APARECIDA",
+            APARECIDA.search(stream).group(0),
+        )
+        colemar = re.sub(
+            r"([0-9]{1,3}[MTN][0-9]{2,4})",
+            r"\1;COLEMAR",
+            COLEMAR.search(stream).group(0),
+        )
+        samambaia = re.sub(
+            r"([0-9]{1,3}[MTN][0-9]{2,4})",
+            r"\1;SAMAMBAIA",
+            SAMAMBAIA.search(stream).group(0),
+        )
+        with open(CSVfile, "w") as CSV:
+            print(aparecida + colemar + samambaia, file=CSV)
 
 
 def main():
-    print(parse_PDF("pdfs/2022-02.pdf"))
-    # print(parse_PDF("pdfs/2023-01.pdf"))
-    # print(parse_PDF("pdfs/2024-01.pdf"))
+    parse_PDF("2024", "01")
 
 
 if __name__ == "__main__":
