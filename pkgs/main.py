2352c160ec75fff5224f64b2c04ef8dfd7ad2877
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-11-23 22:07:37 -0300

    adicionar função que checa se as aulas ocorrem todos no mesmo turno

--- pkgs/main.py
+++ pkgs/main.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import re
+from operator import itemgetter
+from itertools import groupby, combinations
+
 from data.hpw.twelve import CHOICE
+
 
 
 def COMPONENTS_TIMETABLE(timetable):
@@ -18,7 +22,7 @@ def HPW(timetable):
 
 def INDIVIDUAL_DIGITS(number):
     RE_DIGITS = re.compile(r"([1-6])")
-    return [int(x) for x in RE_DIGITS.split(number) if x != ""]
+    return sorted([int(x) for x in RE_DIGITS.split(number) if x != ""])
 
 
 def WEEKDAYS(timetable):
@@ -61,15 +65,75 @@ def CLASSES_OCCUR_IN_THE_SAME_DAYS(timetable1, timetable2):
     return COMP1[0] == COMP2[0]
 
 
-def CLASSES_OCCUR_IN_THE_SAME_PERIOD(timetable1, timetable2):
-    COMP1 = COMPONENTS_TIMETABLE(timetable1)
-    COMP2 = COMPONENTS_TIMETABLE(timetable2)
-    return COMP1[1] == COMP2[1]
+def CLASSES_OCCUR_IN_THE_SAME_PERIOD(choice):
+    N = len(choice) // 5
+    X = sorted([COMPONENTS_TIMETABLE(choice[5 * i + 3])[1] for i in range(N)])
+    return len(list(map(lambda x: itemgetter(0)(x), groupby(X)))) == 1
+
+
+def DISCIPLINES_THAT_I_LIKE(choice):
+    FIELD = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        32,
+        33,
+        34,
+        37,
+        46,
+        47,
+        48,
+    ]
+    N = len(choice) // 5
+    X = [choice[i * 5 + 2] for i in range(N)]
+    return len([x for x in X if x in FIELD]) == N
+
+
+def OVERLAPPING(choice):
+    N = len(choice) // 5
+    X = [choice[i * 5 + 3] for i in range(N)]
+    B = 1
+    for X1, X2 in combinations(X, 2):
+        COMP1 = COMPONENTS_TIMETABLE(X1)
+        COMP2 = COMPONENTS_TIMETABLE(X2)
+        D10 = INDIVIDUAL_DIGITS(COMP1[0])
+        D20 = INDIVIDUAL_DIGITS(COMP2[0])
+        D11 = INDIVIDUAL_DIGITS(COMP1[2])
+        D21 = INDIVIDUAL_DIGITS(COMP2[2])
+        C0 = len([x for x in D10 if x in D20]) > 0
+        C1 = COMP1[1] == COMP2[1]
+        C2 = len([x for x in D11 if x in D21]) > 0
+        B *= 1 if (C0 and C1 and C2) else 0
+    return True if (B == 1) else False
 
 
 def main():
-    for choice in CHOICE:
-        print(choice)
+    CHC = [
+        x
+        for x in CHOICE
+        if DISCIPLINES_THAT_I_LIKE(x) is True and OVERLAPPING(x) is False
+        and CLASSES_OCCUR_IN_THE_SAME_PERIOD(x) is True
+    ]
+
+    with open("test.txt", "w") as tfile:
+        for choice in CHC:
+            print(choice, file=tfile)
 
 
 if __name__ == "__main__":
