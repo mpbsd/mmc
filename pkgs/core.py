87950007fa5327d9834e28215a2eee9318e42071
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-12-08 08:15:07 -0300

    ordenar escolhas priorizando: 1) único campus 2) horários contíguos

--- pkgs/core.py
+++ pkgs/core.py
@@ -39,38 +39,14 @@ def NAME(connection, table, pkey):
     return name
 
 
-def FETCH_TIMETABLE_COMPONENTS(connection, pkey_timetable):
-    re_timetable = re.compile(r"^([0-9]{1,3})([mtn])([0-9]{1,3})$", re.I)
-    timetable = NAME(connection, "horario", pkey_timetable)
-    components = None
-    if (timetable is not None) and (re_timetable.match(timetable) is not None):
-        components = re_timetable.match(timetable).groups()
-    return components
-
-
-def HOURS_PER_WEEK(connection, pkey_timetable):
-    C = FETCH_TIMETABLE_COMPONENTS(connection, pkey_timetable)
-    M = len(C[0])
-    N = len(C[2])
-    return M * N
-
-
-def DIGITS(number):
-    re_digits = re.compile(r"^[0-9]+$")
-    digits = None
-    if re_digits.match(number):
-        digits = [x for x in number]
-    return digits
-
-
 def BLOB(connection, profile):
     pkey_semester = PKEY(connection, "semestre", SEMESTER())
     RE = {
         # 4HPW {{{
-        "4HPW": r"^[0-9]{2}[mtn][0-9]{2}$",
+        "4HPW": r"^[2-6]{2}[mtn][1-6]{2}$",
         # }}}
         # 6HPW {{{
-        "6HPW": r"^[0-9]{3}[mtn][0-9]{2}$",
+        "6HPW": r"^[2-6]{3}[mtn][1-6]{2}$",
         # }}}
     }
     QUERY = {
@@ -340,7 +316,61 @@ def BLOB(connection, profile):
     return blob
 
 
-def RANK(score, blob):
+def FETCH_TIMETABLE_COMPONENTS(connection, pkey_timetable):
+    re_timetable = re.compile(r"^([2-6]{1,3})([mtn])([1-6]{1,3})$", re.I)
+    timetable = NAME(connection, "horario", pkey_timetable)
+    components = None
+    if (timetable is not None) and (re_timetable.match(timetable) is not None):
+        components = re_timetable.match(timetable).groups()
+    return components
+
+
+def FETCH_HOURS_PER_WEEK(connection, pkey_timetable):
+    C = FETCH_TIMETABLE_COMPONENTS(connection, pkey_timetable)
+    M = len(C[0])
+    N = len(C[2])
+    return M * N
+
+
+def DIGITS(number):
+    re_digits = re.compile(r"^[0-9]+$")
+    digits = None
+    if re_digits.match(number):
+        digits = [int(x) for x in number]
+    return digits
+
+
+def DISCIPLINES_TAKE_PLACE_IN_A_SINGLE_CAMPUS(blob):
+    N = len(blob) // 5
+    return True if len(set([blob[i * 5] for i in range(N)])) == 1 else False
+
+
+def DISCIPLINES_ARE_CONTIGUOUS(connection, blob):
+    N = len(blob) // 5
+    T = sorted(
+        [
+            FETCH_TIMETABLE_COMPONENTS(connection, blob[i * 5 + 3])
+            for i in range(N)
+        ],
+        key=lambda x: x[2]
+    )
+    if N > 1:
+        B0 = 1
+        for i in range(N - 1):
+            if (T[i][0] == T[i + 1][0]) and (T[i][1] == T[i + 1][1]):
+                D1 = DIGITS(T[i][2])
+                D2 = DIGITS(T[i + 1][2])
+                if (D1[-1] + 1 == D2[0]):
+                    B0 *= 1
+                else:
+                    B0 *= 0
+        B = True if (B0 == 1) else False
+    else:
+        B = True
+    return T, B
+
+
+def RANK(connection, score, blob):
     N = len(blob) // 5
     S = 0
     for i in range(N):
@@ -349,7 +379,11 @@ def RANK(score, blob):
         score_disciplina = score["disciplina"][blob[i * 5 + 2]]
         score_horario = score["horario"][blob[i * 5 + 3]]
         S += score_campus + score_curso + score_disciplina + score_horario
-    return S
+    if DISCIPLINES_TAKE_PLACE_IN_A_SINGLE_CAMPUS(blob) is True:
+        S += 1
+    if DISCIPLINES_ARE_CONTIGUOUS(connection, blob) is True:
+        S += 1
+    return S / N
 
 
 def DECODE(connection, blob):
@@ -383,13 +417,13 @@ def main():
                         pkey = PKEY(connection, table, entry)
                         score[table][pkey] = toml_file[table][entry]["score"]
 
-        blob = BLOB(connection, 8)
+        blob = BLOB(connection, 12)
 
         with open("unsorted.txt", "w") as unsorted_results:
             for b in blob:
                 print(DECODE(connection, b), file=unsorted_results)
 
-        blob = sorted(blob, key=lambda b: RANK(score, b), reverse=True)
+        blob = sorted(blob, key=lambda b: RANK(connection, score, b), reverse=True)
 
         with open("sorted.txt", "w") as sorted_results:
             for b in blob:
